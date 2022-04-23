384c4471791195667257bbac6ae01b63572a0498
sova02 <heh.20012@mail.ru>
2022-04-23 15:44:56 +0700

    Скрипты SampleManager, RotationScript от Насти

--- Assets/LearnMaterials 2/Scripts/RotationScript.cs
+++ Assets/LearnMaterials 2/Scripts/RotationScript.cs
@@ -4,8 +4,41 @@ using UnityEngine;
 
 public class RotationScript : SampleScript
 {
+    [Range(0, 100)]
+    public float speed = 0;
+    [SerializeField]
+    private Vector3 targetRotE;
+    private Quaternion startRot;
+    private Quaternion targetRot;
+    private bool rotationKey;
+    private float currentDegree;
+    private float totalDegree;
+
+    private void Start()
+    {
+        startRot = transform.rotation;
+        targetRot = Quaternion.Euler(targetRotE);
+        totalDegree = Quaternion.Angle(startRot, targetRot);
+    }
+
+    private void Update()
+    {
+        if (rotationKey)
+        {
+            currentDegree += Time.deltaTime * speed;
+            transform.rotation = Quaternion.Lerp(startRot, targetRot, currentDegree/totalDegree);
+
+            if (currentDegree >= totalDegree)
+            {
+                rotationKey = false;
+            }
+             //Around(transform.position, transform.up, Time.deltaTime * speed);
+        }
+    }
+
+    [ContextMenu("Начать")]
     public override void Use()
     {
-
+        rotationKey = true;
     }
 }
