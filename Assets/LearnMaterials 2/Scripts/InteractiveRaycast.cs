ee020e430fa881f266aeda8a92086a76b5e8d459
LanCore <90953790+LanCore-hub@users.noreply.github.com>
2023-11-20 13:12:43 +0700

    нагн

---
+++ Assets/LearnMaterials 2/Scripts/InteractiveRaycast.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveRaycast : MonoBehaviour
+{
+    public GameObject prefab;
+    private InteractiveBox savedBox;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left click pressed
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("InteractivePlane"))
+                {
+                    Vector3 position = hit.point + hit.normal * (prefab.transform.localScale.x / 2);
+                    GameObject newBox = Instantiate(prefab, position, Quaternion.identity);
+                }
+                else if (hit.collider.CompareTag("InteractiveBox"))
+                {
+                    InteractiveBox clickedBox = hit.collider.GetComponent<InteractiveBox>();
+
+                    if (savedBox == null)
+                    {
+                        savedBox = clickedBox;
+                    }
+                    else
+                    {
+                        savedBox.AddNext(clickedBox);
+                    }
+                }
+            }
+        }
+        else if (Input.GetMouseButtonDown(1)) // Right click pressed
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("InteractiveBox"))
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
